1d8c3f57bf21edd90ba5ea99aa2cc2bb0b30c578
Unicatm <alexandra.tertiscu@gmail.com>
2026-01-14 13:33:55 +0200

    basic controllers & routes + auth

---
+++ backend/src/routes/applicationsRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const applicationsController = require("../controllers/applicationsController");
+const { verifyToken, checkRole } = require("../middlewares/auth");
+
+const router = express.Router();
+
+router.post(
+  "/",
+  verifyToken,
+  checkRole("candidate"),
+  applicationsController.applyToJob
+);
+router.get("/", verifyToken, applicationsController.getApplications);
+
+module.exports = router;
